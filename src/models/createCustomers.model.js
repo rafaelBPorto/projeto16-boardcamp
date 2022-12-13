d48fcff7fac57e05d09d4f3dea2467125477fe8c
rafaelBporto <rafaelbporto88@gmail.com>
2022-12-13 10:31:06 -0300

    fix: customers acept only numbers in phone and cpf

--- src/models/createCustomers.model.js
+++ src/models/createCustomers.model.js
@@ -2,8 +2,7 @@ import joi from "joi";
 
 export const createCustomersSchema = joi.object({
     name: joi.string().min(2).trim().required(),
-    phone: joi.string().alphanum().min(10).max(11).required(),
-    cpf: joi.string().length(11).required(),
+    phone: joi.string().alphanum().pattern(/^[0-9]+$/).min(10).max(11).required(),
+    cpf: joi.string().alphanum().min(11).pattern(/^[0-9]+$/).required(),
     birthday: joi.date().required()
 })
-
