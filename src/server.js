d90eef55eb8fe476f8292038b4226584320ffaa1
rafaelBporto <rafaelbporto88@gmail.com>
2022-12-13 11:39:12 -0300

    feat: cors

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import gamesRoutes from "./routes/games.route.js"
 import customersRoutes from "./routes/customers.route.js"
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(categoriesRoutes);
